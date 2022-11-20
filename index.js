45a33480bd18e793f83666335e02ef1ee311315b
Shashikumar OL <shashikumarol@Shashikumars-MacBook-Air.local>
2022-11-20 17:28:47 +0530

    fix: items api

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const admin = require("./admins/admin-route");
 const agent = require("./agents/agent-route");
+const item = require("./items/item-route");
+const cors = require('cors');
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/admin", admin);
 app.use("/agent", agent)
+app.use("/item", item)
 
 const port = process.env.PORT || 3000;
 
