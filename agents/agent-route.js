1aa02aa2fe71888dbc30cbd1d7639ba6418a4e86
abhishek <oneminddevelopers@gmail.com>
2023-06-19 15:56:07 +0530

    added the get operations for the agents

--- agents/agent-route.js
+++ agents/agent-route.js
@@ -4,93 +4,127 @@ const validate = require("../validate");
 const joi = require("joi");
 const router = express.Router();
 
-router.post('/registration', async (req, res) => {
-    const agent = {
-        name: req.body.name,
-        phone: req.body.phone,
-        email: req.body.email,
-        nominee: {
-            name: req.body.nominee.name,
-            mobile: req.body.nominee.mobile,
-            address: req.body.nominee.address,
-            relationship: req.body.nominee.relationship
-        },
-    }
-    const validate = await validateUser(agent);
-    if (validate.isValid) {
-        const agentID = req.body.agentID;
-        agent.agentID = agentID;
-        if (agent.agentID === 'NA') {
-            Agents.find({}, async (err, response) => {
-                if (err) {
-                    res.status(500).send("Network error, try again")
-                } else {
-                    let count = response.length;
-                    if (count > 0) {
-                        agent.agentID = `AGT${count + 1}`
-                        console.log("agent ", agent);
-                    } else {
-                        count = 1;
-                        agent.agentID = `AGT${count}`
-                    }
-                    try {
-                        const agents = await addAgents(agent);
-                        res.status(200).send(agents)
-                    } catch (err) {
-                        res.status(err.status).send(err)
-                    }
-                }
-            })
-        }else{
-            try {
-                const agents = await addAgents(agent);
-                res.status(200).send(agents)
-            } catch (err) {
-                res.status(err.status).send(err)
-            }
+router.get("/getAgents", async (req, res) => {
+  try {
+    const users = await getAgents();
+    const agents = users.map((item) => ({
+      agentId: item.agentID,
+      agentName: item.name,
+      agentEmail: item.email,
+    }));
+    res.status(200).send(agents);
+  } catch (error) {
+    res.status(500).json({ message: "Some error", error: error });
+  }
+});
+
+router.post("/registration", async (req, res) => {
+  const agent = {
+    name: req.body.name,
+    phone: req.body.phone,
+    email: req.body.email,
+    nominee: {
+      name: req.body.nominee.name,
+      mobile: req.body.nominee.mobile,
+      address: req.body.nominee.address,
+      relationship: req.body.nominee.relationship,
+    },
+  };
+  const validate = await validateUser(agent);
+  if (validate.isValid) {
+    const agentID = req.body.agentID;
+    agent.agentID = agentID;
+    if (agent.agentID === "NA") {
+      Agents.find({}, async (err, response) => {
+        if (err) {
+          res.status(500).send("Network error, try again");
+        } else {
+          let count = response.length;
+          if (count > 0) {
+            agent.agentID = `AGT${count + 1}`;
+            console.log("agent ", agent);
+          } else {
+            count = 1;
+            agent.agentID = `AGT${count}`;
+          }
+          try {
+            const agents = await addAgents(agent);
+            res.status(200).send(agents);
+          } catch (err) {
+            res.status(err.status).send(err);
+          }
         }
+      });
     } else {
-        res.status(400).send(`Bad Request, ${validate.error}`)
+      try {
+        const agents = await addAgents(agent);
+        res.status(200).send(agents);
+      } catch (err) {
+        res.status(err.status).send(err);
+      }
     }
+  } else {
+    res.status(400).send(`Bad Request, ${validate.error}`);
+  }
 });
 
 const validateUser = (agent) => {
-    const user = {
-        email: agent.email,
-        phone: agent.phone
+  const user = {
+    email: agent.email,
+    phone: agent.phone,
+  };
+
+  const schema = joi.object({
+    email: joi.string().min(10).max(255).required().email(),
+    phone: joi
+      .string()
+      .length(10)
+      .pattern(/^[0-9]+$/)
+      .required(),
+  });
+
+  const result = schema.validate(user);
+  if (result.error) {
+    return {
+      isValid: false,
+      error: result.error.details[0].message,
     };
+  } else {
+    return {
+      isValid: true,
+    };
+  }
+};
 
-    const schema = joi.object({
-        email: joi.string().min(10).max(255).required().email(),
-        phone: joi.string().length(10).pattern(/^[0-9]+$/).required(),
+const getAgents = () => {
+  return new Promise(function (resolve, reject) {
+    Agents.find({}, function (err, response) {
+      if (response) {
+        resolve(response);
+      } else {
+        resolve(false);
+      }
     });
-
-    const result = schema.validate(user);
-    if (result.error) {
-        return {
-            isValid: false,
-            error: result.error.details[0].message
-        }
-    } else {
-        return {
-            isValid: true,
-        }
-    }
-}
+  });
+};
 
 const addAgents = (agent) => {
-    return new Promise((resolve, reject) => {
-        const newAgent = new Agents(agent);
-        newAgent.save((err, user) => {
-            if (err) {
-                if (err.index === 0 && err.code === 11000) {
-                    reject({ status: 422, message: 'User already exist!', keyPattern: err.keyPattern});
-                }
-            } else {
-                resolve(user)
-            }
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    const newAgent = new Agents(agent);
+    newAgent.save((err, user) => {
+      if (err) {
+        if (err.index === 0 && err.code === 11000) {
+          reject({
+            status: 422,
+            message: "User already exist!",
+            keyPattern: err.keyPattern,
+          });
+        }
+      } else {
+        resolve(user);
+      }
+    });
+  });
+};
 
 module.exports = router;
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use("/agent", agent)
 app.use("/item", item)
 app.use("/customers", customer)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
     res.send("Hello NodeJS World!...")
