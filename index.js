c878776f24a4e1ca535803caa66bdba1b05cabb0
abhishek <oneminddevelopers@gmail.com>
2023-06-26 00:24:07 +0530

    pushed the deployment changes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const admin = require("./admins/admin-route");
 const agent = require("./agents/agent-route");
 const item = require("./items/item-route");
-const customer = require("./customers/customer-route")
-const cors = require('cors');
+const customer = require("./customers/customer-route");
+const cors = require("cors");
 
 const app = express();
 
@@ -13,16 +13,26 @@ app.use(cors());
 
 // Routes
 app.use("/admin", admin);
-app.use("/agent", agent)
-app.use("/item", item)
-app.use("/customers", customer)
+app.use("/agent", agent);
+app.use("/item", item);
+app.use("/customers", customer);
 
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send("Hello NodeJS World!...")
+app.get("/", (req, res) => {
+  res.send("Hello NodeJS World!...");
+});
+
+app.use(
+  express.static(path.join(__dirname, "./LICAGENTMANAGEMENT-FRONTEND/build"))
+);
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "./LICAGENTMANAGEMENT-FRONTEND/build/index.html")
+  );
 });
 
 app.listen(port, () => {
-    console.log(`Server running at port ${port} `)
+  console.log(`Server running at port ${port} `);
 });
